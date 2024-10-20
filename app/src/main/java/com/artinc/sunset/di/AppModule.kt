c79cbbb01c61fde05fe715553c4f0c0bc86f5eab
artS3rg <art.serg.3002@mail.ru>
2024-10-20 21:00:33 +0300

    update

--- app/src/main/java/com/artinc/sunset/di/AppModule.kt
+++ app/src/main/java/com/artinc/sunset/di/AppModule.kt
@@ -9,15 +9,32 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val appModule = module {
-    single { Retrofit.Builder()
-        .baseUrl("https://api.openweathermap.org/data/2.5/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(OkHttpClient())
-        .build()
-        .create(WeatherApiService::class.java)
+
+    // Создание кастомного OkHttpClient с тайм-аутами
+    single {
+        OkHttpClient.Builder()
+            .connectTimeout(20, TimeUnit.SECONDS)  // Тайм-аут на подключение
+            .readTimeout(20, TimeUnit.SECONDS)     // Тайм-аут на чтение
+            .writeTimeout(20, TimeUnit.SECONDS)    // Тайм-аут на запись
+            .build()
     }
+
+    // Retrofit с настроенным OkHttpClient
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())  // Передача кастомного OkHttpClient
+            .build()
+            .create(WeatherApiService::class.java)
+    }
+
+    // Репозиторий
     single<WeatherRepository> { WeatherRepositoryImpl(get()) }
+
+    // Презентер
     viewModel { WeatherPresenter(get()) }
 }
--- app/src/main/java/com/artinc/sunset/ui/WeatherScreen.kt
+++ app/src/main/java/com/artinc/sunset/ui/WeatherScreen.kt
@@ -2,34 +2,43 @@ package com.artinc.sunset.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
+import com.artinc.sunset.R
 import com.artinc.sunset.presenter.WeatherPresenter
-import androidx.compose.runtime.livedata.observeAsState
 
 @Composable
 fun WeatherScreen(viewModel: WeatherPresenter) {
     var city by remember { mutableStateOf("") }
     val weatherState = viewModel.weatherState.collectAsState()
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center
+    ) {
         TextField(
             value = city,
             onValueChange = { city = it },
-            label = { Text("Enter city") },
+            label = { Text(stringResource(R.string.search_edittext)) },
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
             modifier = Modifier.fillMaxWidth()
         )
         Button(
             onClick = { viewModel.getWeather(city) },
             modifier = Modifier.fillMaxWidth()
         ) {
-            Text("Get Weather")
+            Text(stringResource(R.string.search_btn))
         }
 
         weatherState.value?.let { weather ->
